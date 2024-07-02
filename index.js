be84c712be93b63b56d9933ef7e7e3fb93f0ea29
KennuByte <73755588+kennu290@users.noreply.github.com>
2024-07-02 08:58:26 +0300

    added stuff

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+console.clear();
 
-app.use(express.static(path.join(__dirname, 'public')));
+function isLoopbackAddress(ip) {
+    return ip === '::1' || ip === '127.0.0.1' || ip === '::ffff:127.0.0.1';
+}
 
+app.get('/*', (req, res) => {
+    var file = req.params[0] ? req.params[0] : 'index.html';
+    const clientIP = req.ip;
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    if (isLoopbackAddress(clientIP)) {
+        res.sendFile(path.join(__dirname, 'public', file))
+    } else {
+        res.status(403).send('Forbidden');
+    }
 });
 
-
-const PORT = process.env.PORT || 3000;
+const PORT = 31234;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
+    console.log('Press Ctrl+C to stop');
 });
